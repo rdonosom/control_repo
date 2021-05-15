1f2d78c32088bff0a95b270de422fecbfde99a3f
rdonosom <44450057+rdonosom@users.noreply.github.com>
2021-05-15 17:26:23 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,7 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC5fbK7x2V4gSAbQ8Qdy1f/AZwKvkQ9J0jhmodiAp/oU6zm1fZdwJpufIQhYFG6foOY9wo2yAZgzlY+8zGyNCi4mlwb51ajSgSL0CFHGM6jndVLV2rCL6PB2WZ9Boddf2j1ParbEwRqtkb+cr5/AQUaOlcQzaFxQTKuHkpfxBlMNRf+xrsE+NGAyMS8IpNEc9ShHaDcQ
-LMS2nzpxUdeXKMRhI8PfZ3Dn9Fs3kUoL+dbP8zjG33y4bbJlpReBLnL5kVVxm9m97ENkhPgvKul6Ncd/DvwKXszIVi728K8mWhpJAI4SZ/G/x4lmFo6TJ0j1bjeFL6SJo6hTv2+qYXfmbEZ',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC5fbK7x2V4gSAbQ8Qdy1f/AZwKvkQ9J0jhmodiAp/oU6zm1fZdwJpufIQhYFG6foOY9wo2yAZgzlY+8zGyNCi4mlwb51ajSgSL0CFHGM6jndVLV2rCL6PB2WZ9Boddf2j1ParbEwRqtkb+cr5/AQUaOlcQzaFxQTKuHkpfxBlMNRf+xrsE+NGAyMS8IpNEc9ShHaDcQLMS2nzpxUdeXKMRhI8PfZ3Dn9Fs3kUoL+dbP8zjG33y4bbJlpReBLnL5kVVxm9m97ENkhPgvKul6Ncd/DvwKXszIVi728K8mWhpJAI4SZ/G/x4lmFo6TJ0j1bjeFL6SJo6hTv2+qYXfmbEZ',
 	}  
 }
