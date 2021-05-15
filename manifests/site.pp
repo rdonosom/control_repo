98804ddd2303f421448426208bd567e9e828d6f0
rdonosom <44450057+rdonosom@users.noreply.github.com>
2021-05-15 14:05:49 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'Ejemplo de puppet content',
-    owner => 'root',
-    }
-    file {'/root/README':
-        owner => 'root',
-    }  
 }
